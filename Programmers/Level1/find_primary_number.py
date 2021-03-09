d97b238af400731d446a6dad0e9820773f9b901c
dydfuf <88dydfuf@naver.com>
2021-03-09 22:23:43 +0900

    소수찾기 풀이 추가

--- Programmers/Level1/find_primary_number.py
+++ Programmers/Level1/find_primary_number.py
@@ -1,23 +1,31 @@
 def solution(n):
     answer = 0
-    dp = [True] * (n+1)
+    dp = [True] * (n + 1)
 
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         if dp[i]:
             answer += 1
             dp[i] = False
             temp = i
             idx = 1
-            while temp*idx <= n:
-
-                dp[temp*idx] = False
+            while temp * idx <= n:
+                dp[temp * idx] = False
                 idx += 1
 
     return answer
 
 
+def solution2(n):
+    num = set(range(2, n + 1))
+
+    for i in range(2, n + 1):
+        if i in num:
+            num -= set(range(2 * i, n + 1, i))
+    return len(num)
+
+
 if __name__ == "__main__":
     n = 10
-    print(solution(n))
+    print(solution2(n))
     n = 5
-    print(solution(n))
+    print(solution2(n))
