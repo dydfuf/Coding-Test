3a0109bbab241686713a0f3c2fbebcf802b469d5
dydfuf <88dydfuf@naver.com>
2021-03-11 07:23:32 +0900

    프린터 답안 추가

--- Programmers/Level2/printer.py
+++ Programmers/Level2/printer.py
@@ -22,6 +22,19 @@ def solution(priorities, location):
     return answer
 
 
+def solution2(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+
+
 if __name__ == "__main__":
     arr_priorities = [[2, 1, 3, 2], [1, 1, 9, 1, 1, 1]]
     arr_location = [2, 0]
