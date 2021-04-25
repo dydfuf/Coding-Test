ab129e308b915168e3705b4e0f053e49bec7fe19
dydfuf <88dydfuf@naver.com>
2021-04-25 23:03:31 +0900

    내적.js

---
+++ Programmers/Level1/dot_product.js
@@ -0,0 +1,17 @@
+function solution(a, b) {
+    let answer = 0
+
+    for(let i = 0; i < a.length; i++){
+        answer += a[i] * b[i]
+    }
+
+    return answer;
+}
+
+function main(){
+    let a = [1,2,3,4]
+    let b = [-3,-1,0,2]
+    console.log(solution(a,b))
+}
+
+main()
