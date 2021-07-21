b37122e805a4256f79b450fe46f2b1f1964f6831
dydfuf <88dydfuf@naver.com>
2021-07-22 04:30:23 +0900

    [10973] 이전 순열

---
+++ BOJ/SDS대비/prev_permutation.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(input())
+    data = [i + 1 for i in range(N)]
+    target = list(map(int, input().split()))
+
+    find = False
+    for i in range(N-1, 0, -1):
+        if target[i] < target[i-1]:
+            for j in range(N-1, 0, -1):
+                if target[j] < target[i-1]:
+                    target[i-1], target[j] = target[j], target[i-1]
+                    target = target[:i] + sorted(target[i:], reverse=True)
+                    find = True
+                    break
+        if find:
+            print(*target)
+            break
+    if not find:
+        print(-1)
