67de17380df4dc28032a41c1f08b02fcc4403f06
dydfuf <88dydfuf@naver.com>
2021-07-24 03:35:36 +0900

    [15664] N과 M (10)

--- BOJ/SDS대비/n_and_m_9.py
+++ BOJ/SDS대비/n_and_m_9.py
@@ -1,12 +1,12 @@
 import sys
-from itertools import permutations
+from itertools import combinations
 input = sys.stdin.readline
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
     data = sorted(list(map(int, input().split())))
 
-    perm = sorted(list(set(permutations(data, M))))
+    perm = sorted(list(set(combinations(data, M))))
 
     for aPerm in perm:
         print(*aPerm)
